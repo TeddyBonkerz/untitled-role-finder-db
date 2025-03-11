19177312d4ee928e63492108a5b70cf2ccae9cad
TeddyBonkerz <33665345+TeddyBonkerz@users.noreply.github.com>
2025-03-10 20:36:36 -0400

    PA: initial commit for job roles database

---
+++ Docker/README.md
@@ -0,0 +1,11 @@
+- Ensure you have Python install and setup if your ide of choice.
+
+- Start mongodb container for Role Finder DB image.
+    cd Docker
+    docker compose up
+
+To log into the container
+- docker exec -it role_documentdb bash
+- mongosh "mongodb://{username}:{password}@localhost:27017/?authSource=adm{authsource}
+
+Then run mongodb commands as needed.
---
+++ src/role_findeer.py
@@ -0,0 +1,49 @@
+import json
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+
+def connect_to_database():
+    
+    try:
+        host = "localhost"
+        port = 27017
+        db_name = "mongo_park_db"
+        db_collection = "mongo_park_collection"
+        username = quote_plus("mongo_park")
+        password = quote_plus("mongo_park")
+        authsource = "admin"
+
+        uri = f"mongodb://{username}:{password}@{host}:{port}/?authSource=adm{authsource}"
+
+
+        client = MongoClient(uri)
+        client.admin.command("ping")
+        print("Connected successfully")
+
+
+        role_db = client[db_name]
+        role_collection = role_db.get_collection(db_collection)
+
+        with open("job_roles_dump.json", 'r') as file:
+            roles = json.load(file)
+        
+        # Validate JSON structure
+        if not isinstance(roles, list):
+            raise ValueError("JSON file must contain a list of role descriptions.")
+        
+        result = role_collection
+
+        result = role_collection.insert_many(roles)
+        print(f"Inserted document ID: {result.inserted_id}")
+
+    except Exception as e:
+        raise Exception(
+            "The following error occurred: ", e)
+    
+    finally:
+        client.close()
+    
+if __name__ == "__main__":   
+  
+   # Get the database
+   connect_to_database()
